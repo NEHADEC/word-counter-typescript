5d4708b9b71afe992068985789d35a0bc5937634
NEHADEC <faizanbilal472@gmail.com>
2024-06-05 07:10:39 -0700

    Word-Counter Typescript

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/dev/env node
+
+// Importing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Display a colourful welcome message
+
+console.log(chalk.bold.cyanBright(` /n /t/t code with neha - word counter`));
+console.log("=".repeat(60));
+
+//prompt the user to enter a sentance
+let answers = await inquirer.prompt([
+    {
+        name : "sentence",
+        type: "input",
+        message: "Enter a sentence",
+    }
+]);
+
+//trimmimg the sentance and splitting it into words based on "space"
+let words = answers.sentence.trim().split("");
+
+//Analysis of user input sentance
+
+console.log("=".repeat(60));
+
+console.log(chalk.bold("- sentance words:"));
+
+console.log(words);
+
+console.log(chalk.bold(`\n = word count: ${chalk.bold.redBright(words.length)}`));
+
+console.log("=".repeat(60));
+
+
+
+
+
+
